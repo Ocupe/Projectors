2f46883605f45560e2964b97d546828126d2cb87
ocupe <jonas@livekit.io>
2024-03-10 19:29:09 +0100

    Update Projector version in __init__.py

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ from . import projector
 from . import operators
 
 bl_info = {
-    "name": "Projector RC1",
+    "name": "Projector",
     "author": "Jonas Schell",
     "description": "Easy Projector creation and modification.",
     "blender": (2, 81, 0),
-    "version": (2024, 0, 0),
+    "version": (2024, 1, 0),
     "location": "3D Viewport > Add > Light > Projector",
     "category": "Lighting",
     "wiki_url": "https://github.com/Ocupe/Projectors/wiki",
